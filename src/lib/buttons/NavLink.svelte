0fc073ad71a5838f11f0af0ac64501b86f3f434a
Bryceson Laing <bklaing2@gmail.com>
2025-06-09 00:23:07 -0500

    perfected the feel of the header transitions

--- src/lib/buttons/NavLink.svelte
+++ src/lib/buttons/NavLink.svelte
@@ -1,11 +1,12 @@
 <script lang="ts">
+	import type { Snippet } from 'svelte';
 	import { page } from '$app/state';
 
 	interface Props {
 		href: string;
 		hidden?: boolean;
 		active?: boolean | undefined;
-		children?: import('svelte').Snippet;
+		children: Snippet;
 	}
 
 	let { href, hidden = false, active = undefined, children }: Props = $props();
@@ -16,10 +17,10 @@
 	<a
 		{href}
 		data-sveltekit-noscroll
-		class="rounded-full border px-5 py-2 hover:bg-orange-200/50 hover:text-amber-900 {isActive
+		class="rounded-full border px-5 py-2 text-nowrap hover:text-amber-900 {isActive
 			? 'border-amber-900/20  bg-orange-200/30'
-			: 'border-transparent'} {hidden && 'hidden'}"
+			: 'border-transparent'}  {hidden ? 'hidden' : ''}"
 	>
-		{@render children?.()}
+		{@render children()}
 	</a>
 </li>
--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -1,31 +1,28 @@
 <script lang="ts">
 	import { page } from '$app/state';
-	import NavLink from '$lib/buttons/NavLink.svelte';
 	import { Button } from '$lib/components/ui/button';
+	import NavLink from '$lib/buttons/NavLink.svelte';
 	import MenuIcon from 'virtual:icons/tabler/menu-2';
 	import CloseIcon from 'virtual:icons/tabler/x';
 
 	let reading = $derived(page.url.pathname.includes('/read/'));
-
-	function onClick() {
-		alert('Menu clicked!');
-	}
-
-	let navbarOpen = $state(false);
+	let showNav = $state(false);
 </script>
 
-<header class="relative mx-6 my-6">
+<header class="group my-6 flex w-full items-center px-6">
 	<a
 		href="/"
-		class={`absolute left-0 z-0 flex items-center gap-2 overflow-hidden text-xl text-amber-900 transition-[opacity] ${navbarOpen && 'opacity-0'}`}
+		class={`absolute flex items-center gap-2 overflow-hidden text-xl text-amber-900 transition-all sm:justify-end ${showNav ? 'max-sm:opacity-0' : 'max-sm:delay-100 max-sm:duration-250'}`}
 	>
 		<img src="/favicon.png" alt="optiscape icon" class="aspect-square h-8" />
 		Optiscape
 	</a>
 
-	<div class={`absolute right-0 flex w-full items-center justify-end`}>
+	<div
+		class={`ms-auto flex w-full items-center justify-end overflow-hidden rounded-full transition-all ease-out sm:w-min sm:group-hover:bg-black/3 sm:group-hover:shadow-xl sm:group-hover:shadow-black/3 ${showNav ? 'max-sm:min-w-full max-sm:bg-black/5 max-sm:shadow-xl max-sm:shadow-black/5 max-sm:delay-100 max-sm:duration-1000' : 'max-sm:min-w-0'}`}
+	>
 		<nav
-			class="{`scrollbar-hidden overflow-x-auto transition-[max-width] ${navbarOpen ? 'max-w-96' : 'max-sm:max-w-0'} flex gap-x-1 sm:max-w-96`},"
+			class={`flex  scrollbar-hidden grow gap-x-1 overflow-x-auto transition-all ease-out sm:max-w-500 ${showNav ? 'max-sm:max-w-500 max-sm:delay-100 max-sm:duration-1000' : 'max-sm:max-w-0'}`}
 		>
 			<ul class="contents">
 				<NavLink href="/">Home</NavLink>
@@ -37,11 +34,11 @@
 		</nav>
 
 		<Button
-			onclick={() => (navbarOpen = !navbarOpen)}
-			class="text-xl text-rose-700 sm:hidden"
+			onclick={() => (showNav = !showNav)}
+			class="flex-none text-xl text-rose-700 sm:hidden"
 			variant="ghost"
 		>
-			{#if navbarOpen}
+			{#if showNav}
 				<CloseIcon />
 			{:else}
 				<MenuIcon />
