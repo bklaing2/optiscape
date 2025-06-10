7e538beb8b481620431a25e75d4a25409199a89a
Bryceson Laing <bklaing2@gmail.com>
2025-06-10 15:26:48 -0500

    updated main layout to flow better

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,19 +1,21 @@
 <script lang="ts">
-	import type { LayoutProps } from './$types';
-	import '../app.css';
-	import './styles.css';
-	import Header from './Header.svelte';
-	import Footer from './Footer.svelte';
+	import type { LayoutProps } from './$types'
+	import '../app.css'
+	import './styles.css'
+	import Header from './Header.svelte'
+	import Footer from './Footer.svelte'
 
-	let { children }: LayoutProps = $props();
+	let { children }: LayoutProps = $props()
 </script>
 
 <div
-	class="mx-auto grid h-full max-w-4xl grid-cols-1 grid-rows-[min-content_minmax(min-content,1fr)_min-content] gap-8 pb-16"
+	class="mx-auto grid max-w-4xl grid-cols-1 grid-rows-[min-content_minmax(min-content,1fr)_min-content] gap-8 pb-16"
 >
 	<Header />
 
-	<main class="box-border flex w-full flex-col px-4">{@render children?.()}</main>
+	<main class="box-border flex w-full flex-col px-4">
+		{@render children()}
+	</main>
 
 	<Footer signedIn={false} />
 </div>
