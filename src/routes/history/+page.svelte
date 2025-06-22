b460ff00c44ab71d215834cace10cc0b4301fff8
Bryceson Laing <bklaing2@gmail.com>
2025-06-22 13:00:56 -0500

    update history page

--- src/routes/history/+page.svelte
+++ src/routes/history/+page.svelte
@@ -1,10 +1,18 @@
 <script lang="ts">
-	import type { PageProps } from './$types';
-	import BookList from '$lib/book/List.svelte';
+  import type { Book } from '$lib/types/types'
+  import { onMount } from 'svelte'
+  import { getHistory } from '$lib/db'
+  import BookView from '$lib/book/Book.svelte'
 
-	let { data }: PageProps = $props();
-	let { history } = $derived(data);
+  let history = $state([] as Book[])
+
+  onMount(async () => (history = await getHistory()))
 </script>
 
-<h1>Optiscape books</h1>
-<BookList books={history} height={180} className="col-span-full" />
+<div class="col-span-full grid grid-cols-3 gap-x-2 sm:grid-cols-5">
+  <h1 class="col-span-full text-xl">History</h1>
+
+  {#each history as book (book.id)}
+    <BookView {book} class="w-full" />
+  {/each}
+</div>
--- src/routes/history/+page.ts
+++
@@ -1,6 +0,0 @@
-import type { PageLoad } from './$types';
-import storage from '$lib/util/storage';
-
-export const ssr = false
-
-export const load: PageLoad = async () => ({ history: storage.loadHistory() })
