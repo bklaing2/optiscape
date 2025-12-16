c2ccf3d609e59156888e305f7893d9cb3ad8ca07
Bryceson Laing <bklaing2@gmail.com>
2025-12-15 21:55:44 -0600

    Revert "don't prerender"
    This reverts commit 82079541baa0204c26ac108bf4b402b2bdb69a93.

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -6,8 +6,6 @@ import { XML } from 'r2-utils-js/dist/es8-es2017/src/_utils/xml-js-mapper'
 import { OPDS } from 'r2-opds-js/dist/es8-es2017/src/opds/opds1/opds'
 import { EntryToBook } from '$lib/util/misc'
 
-export const prerender = false;
-
 export const load: PageServerLoad = async ({ locals }) => {
   const { fetchBooks } = locals;
 
