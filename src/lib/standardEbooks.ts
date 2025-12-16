ca0a9c39f2fe40fc646eec8516cbcf5076f4b08d
Bryceson Laing <bklaing2@gmail.com>
2025-12-15 20:17:40 -0600

    use private environment variable

--- src/lib/standardEbooks.ts
+++ src/lib/standardEbooks.ts
@@ -1,7 +1,7 @@
-import { PUBLIC_STANDARD_EBOOKS_KEY } from '$env/static/public'
+import { STANDARD_EBOOKS_KEY } from '$env/static/private'
 
 const STANDARD_EBOOKS_URL = 'https://standardebooks.org/feeds/opds'
-const STANDARD_EBOOKS_OPTIONS = { headers: { 'User-Agent': PUBLIC_STANDARD_EBOOKS_KEY } }
+const STANDARD_EBOOKS_OPTIONS = { headers: { 'User-Agent': STANDARD_EBOOKS_KEY } }
 
 const fetchBooks = (feed = "") => fetch(`${STANDARD_EBOOKS_URL}/${feed}`, STANDARD_EBOOKS_OPTIONS)
 
