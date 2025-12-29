9fb38574dfd66f3c79b11bde4dee64ff02e04736
Bryceson Laing <bklaing2@gmail.com>
2025-12-29 15:42:03 -0600

    refactor props to extend `HTMLAnchorAttributes`

--- src/lib/buttons/NavLink.svelte
+++ src/lib/buttons/NavLink.svelte
@@ -1,26 +1,29 @@
+<script lang="ts" module>
+  import type { HTMLAnchorAttributes } from 'svelte/elements'
+
+  interface Props extends HTMLAnchorAttributes {
+    hidden?: boolean
+    active?: boolean
+  }
+</script>
+
 <script lang="ts">
-	import type { Snippet } from 'svelte';
-	import { page } from '$app/state';
+  import { page } from '$app/state'
 
-	interface Props {
-		href: string;
-		hidden?: boolean;
-		active?: boolean | undefined;
-		children: Snippet;
-	}
-
-	let { href, hidden = false, active = undefined, children }: Props = $props();
-	let isActive = $derived(active !== undefined ? active : page.url.pathname === href);
+  let { hidden = false, active = undefined, ...props }: Props = $props()
+  let isActive = $derived(
+    active !== undefined ? active : page.url.pathname === props.href
+  )
 </script>
 
 <li class="contents" aria-current={active ? 'page' : undefined}>
-	<a
-		{href}
-		data-sveltekit-noscroll
-		class="rounded-full border px-5 py-2 text-nowrap hover:text-amber-900 {isActive
-			? 'border-amber-900/20  bg-orange-200/30'
-			: 'border-transparent'}  {hidden ? 'hidden' : ''}"
-	>
-		{@render children()}
-	</a>
+  <a
+    data-sveltekit-noscroll
+    class="rounded-full border px-5 py-2 text-nowrap hover:text-amber-900 {isActive
+      ? 'border-amber-900/20  bg-orange-200/30'
+      : 'border-transparent'}  {hidden ? 'hidden' : ''}"
+    {...props}
+  >
+    {@render props.children?.()}
+  </a>
 </li>
